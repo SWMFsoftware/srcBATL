6c1a1e933d7cdd09e4531746e7e80485e297b618
Gabor Toth <gtoth@csrwks2022-0212.engin.umich.edu>
2024-11-05 16:48:27 -0500

    FormatFortran

--- BATL_pass_cell_gpu_parallel.f90
+++ BATL_pass_cell_gpu_parallel.f90
@@ -771,7 +771,7 @@ contains
 
     ! set memory maps for parallel algorithm:
     ! nMsgSend_PBI,  iBufferS_IPI, iMsgDir_IBPI
-    
+
     use BATL_size, ONLY: iRatio, jRatio, kRatio
     use BATL_tree, ONLY: &
          iNodeNei_IIIB, DiLevelNei_IIIB, iNode_B, &
@@ -1364,12 +1364,12 @@ contains
       integer :: DiR, DjR, DkR
 
       integer :: IntDir, iMsgGlob
-      !------------------------------------------------------------------------
 
       ! For sideways communication from a fine to a coarser block
       ! the coordinate parity of the sender block tells
       ! if the receiver block fills into the
       ! lower (D*Recv = 0) or upper (D*Rev=1) half of the block
+      !------------------------------------------------------------------------
       iSide = 0; if(iRatio==2) iSide = modulo(iTree_IA(Coord1_,iNodeSend)-1, 2)
       jSide = 0; if(jRatio==2) jSide = modulo(iTree_IA(Coord2_,iNodeSend)-1, 2)
       kSide = 0; if(kRatio==2) kSide = modulo(iTree_IA(Coord3_,iNodeSend)-1, 2)
